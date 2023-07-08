73a4722d10330dc4bb790490107cae9cf06b5fc7
heeyonhh <lightram23@gmail.com>
2023-07-08 15:32:02 +0900

    first commit

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCkIZnzeqjyiSr2Ezc3c-Q4p7mMjWmhGNY",
+    authDomain: "my-login-app-a3c4b.firebaseapp.com",  
+    projectId: "my-login-app-a3c4b",
+    storageBucket: "my-login-app-a3c4b.appspot.com",
+    messagingSenderId: "422614904517", 
+    appId: "1:422614904517:web:aa174fafa67cbabed94aac", 
+    measurementId: "G-JJRHEK391G" 
+};
